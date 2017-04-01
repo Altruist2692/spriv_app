b99c293c8662dcee4eb054f3a2e1aab72b580059
Altruist2692 <altruist2692@gmail.com>
2017-04-01 18:41:44 +0530

    Add API for Authentication.

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,2 +1,87 @@
+require 'net/http'
+
 class AuthenticationsController < ApplicationController
+  def login
+  end
+
+  def add_login
+    @user = User.find_by_user_login(authentication_params[:user_login])
+    uri = URI('https://m.spriv.com/wsM5.asmx/AddLogin')
+    params = {
+      "strUsername" => ENV['SPRIV_USERNAME'],
+      "strPassword" => ENV['SPRIV_PASSWORD'],
+      "strEndUsername" => @user.user_login,
+      "strPCFingerprint" => request.user_agent,
+      "strIPAddress" => request.ip,
+      "strService" => "Web Access",
+      "nMethod" => 1,
+      "bAsHTML" => false
+    }
+    res = Net::HTTP.post_form(uri, params)
+    JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
+    redirect_to login_path
+    #  {"Code"=>200, "Message"=>"OK", "ID"=>"65a0b79d0fa24519a9355ef7c9380e62", "PollServiceURL"=>"https://sprivservices.azurewebsites.net"}
+  end
+
+  def add_verification
+    @user = User.find_by_user_login(authentication_params[:user_login])
+    uri = URI('https://m.spriv.com/wsM5.asmx/AddVerification')
+    params = {
+      "strUsername" => ENV['SPRIV_USERNAME'],
+      "strPassword" => ENV['SPRIV_PASSWORD'],
+      "strEndUsername" => @user.user_login,
+      "strPCFingerprint" => request.user_agent,
+      "strIPAddress" => request.ip,
+      "strService" => "Web Access",
+      "nMethod" => 1,
+      "bAsHTML" => false,
+      "strMessage" => 'Hello There Naiya'
+    }
+    res = Net::HTTP.post_form(uri, params)
+    JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
+    redirect_to login_path
+  end
+
+  def add_two_way_sms_verification
+    @user = User.find_by_user_login(authentication_params[:user_login])
+    uri = URI('https://m.spriv.com/wsM5.asmx/AddVerification')
+    params = {
+      "strUsername" => ENV['SPRIV_USERNAME'],
+      "strPassword" => ENV['SPRIV_PASSWORD'],
+      "strEndUsername" => @user.user_login,
+      "strPCFingerprint" => request.user_agent,
+      "strIPAddress" => request.ip,
+      "strService" => "Web Access",
+      "nMethod" => 1,
+      "bAsHTML" => false,
+      "strMessage" => 'Hello There Naiya'
+    }
+    res = Net::HTTP.post_form(uri, params)
+    JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
+    redirect_to login_path
+  end
+
+  def add_totp
+    @user = User.find_by_user_login(authentication_params[:user_login])
+    uri = URI('https://m.spriv.com/wsM5.asmx/AddTotp')
+    params = {
+      "strUsername" => ENV['SPRIV_USERNAME'],
+      "strPassword" => ENV['SPRIV_PASSWORD'],
+      "strEndUsername" => @user.user_login,
+      "strKey" => authentication_params[:key],
+      "strIPAddress" => request.ip,
+      "strService" => "Web Access",
+      "nMethod" => 1,
+      "bAsHTML" => false
+    }
+    res = Net::HTTP.post_form(uri, params)
+    JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
+    redirect_to login_path
+  end
+
+  private
+
+  def authentication_params
+    params.require(:authentication).permit!
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
+  get 'users/login' => 'authentications#login', as: :login
+  get "callback_handler" => 'home#callback_handler'
   resources :users
   get 'users/:id/invite' => 'users#invite', as: :invite_user
+  post 'users/add_login' => 'authentications#add_login', as: :add_login
+  post 'users/add_verification' => 'authentications#add_verification', as: :add_verification
+  post 'users/add_two_way_sms_verification' => 'authentications#add_two_way_sms_verification', as: :add_two_way_sms_verification
+  post 'users/add_totp' => 'authentications#add_totp', as: :add_totp
 end
