0f1d22a0de72cb97ddaa97757c446bf4f9790c65
Altruist2692 <altruist2692@gmail.com>
2017-04-08 16:19:21 +0530

    Add changes for validation of otp

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -4,66 +4,83 @@ class AuthenticationsController < ApplicationController
 
   def add_login
     @user = User.find_by_user_login(authentication_params[:user_login])
-    params = {
-      "strEndUsername" => @user.user_login,
-      "strPCFingerprint" => request.user_agent,
-      "strIPAddress" => request.ip,
-      "strService" => "Web Access",
-      "nMethod" => 1,
-      "bAsHTML" => false
-    }
-    response = Spriv::Client.new.add_login(params)
-    response = Spriv::Poller.new.get_decision(response['ID'])
-    set_flash(response)
+    if @user.present?
+      params = {
+        "strEndUsername" => @user.user_login,
+        "strPCFingerprint" => request.user_agent,
+        "strIPAddress" => request.ip,
+        "strService" => "Web Access",
+        "nMethod" => 1,
+        "bAsHTML" => false
+      }
+      response = Spriv::Client.new.add_login(params)
+      response = Spriv::Poller.new.get_decision(response['ID'])
+      set_flash(response)
+    else
+      flash[:danger] = 'Unable to find user with this login'
+    end
     redirect_to login_path
+
   end
 
   def add_verification
     @user = User.find_by_user_login(authentication_params[:user_login])
-    params = {
-      "strEndUsername" => @user.user_login,
-      "strPCFingerprint" => request.user_agent,
-      "strIPAddress" => request.ip,
-      "strService" => "Web Access",
-      "nMethod" => 1,
-      "bAsHTML" => false,
-      "strMessage" => 'Hello There Dev'
-    }
-    response = Spriv::Client.new.add_verification(params)
-    response = Spriv::Poller.new.get_decision(response['ID'])
-    set_flash(response)
+    if @user.present?
+      params = {
+        "strEndUsername" => @user.user_login,
+        "strPCFingerprint" => request.user_agent,
+        "strIPAddress" => request.ip,
+        "strService" => "Web Access",
+        "nMethod" => 1,
+        "bAsHTML" => false,
+        "strMessage" => 'Hello There Dev'
+      }
+      response = Spriv::Client.new.add_verification(params)
+      response = Spriv::Poller.new.get_decision(response['ID'])
+      set_flash(response)
+    else
+      flash[:danger] = 'Unable to find user with this login'
+    end
     redirect_to login_path
   end
 
   def add_two_way_sms_verification
     @user = User.find_by_user_login(authentication_params[:user_login])
-    params = {
-      "strEndUsername" => @user.user_login,
-      "strPCFingerprint" => request.user_agent,
-      "strIPAddress" => request.ip,
-      "strService" => "Web Access",
-      "nMethod" => 2,
-      "bAsHTML" => false,
-      "strMessage" => 'Hello There Naiya!!'
-    }
-    response = Spriv::Client.new.add_verification(params)
-    response = Spriv::Poller.new.get_decision(response['ID'])
-    set_flash(response)
+    if @user.present?
+      params = {
+        "strEndUsername" => @user.user_login,
+        "strPCFingerprint" => request.user_agent,
+        "strIPAddress" => request.ip,
+        "strService" => "Web Access",
+        "nMethod" => 2,
+        "bAsHTML" => false,
+        "strMessage" => 'Hello There Naiya!!'
+      }
+      response = Spriv::Client.new.add_verification(params)
+      response = Spriv::Poller.new.get_decision(response['ID'])
+      set_flash(response)
+    else
+      flash[:danger] = 'Unable to find user with this login'
+    end
     redirect_to login_path
   end
 
   def add_totp
     @user = User.find_by_user_login(authentication_params[:user_login])
-    params = {
-      "strEndUsername" => @user.user_login,
-      "strKey" => authentication_params[:key],
-      "strIPAddress" => request.ip,
-      "strService" => "Web Access",
-      "nMethod" => 1,
-      "bAsHTML" => false
-    }
-    response = Spriv::Client.new.add_totp(params)
-    set_flash(response)
+    if @user.present?
+      params = {
+        "strEndUsername" => @user.user_login,
+        "strKey" => authentication_params[:key],
+        "strIPAddress" => request.ip,
+        "strService" => "Web Access",
+        "nMethod" => 1,
+        "bAsHTML" => false
+      }
+      response = Spriv::Client.new.add_totp(params)
+      set_flash(response)
+    else
+      flash[:danger] = 'Unable to find user with this login'
+    end
     redirect_to login_path
   end
 
@@ -80,6 +97,8 @@ class AuthenticationsController < ApplicationController
       flash[:danger] = "Authentication Denied."
     elsif response['Message'] == 'OK'
       flash[:success] = "Authentication Done successfully"
+    elsif response['Message'].match(/Wrong TOTP/)
+      flash[:danger] = "Wrong TOTP code, please try again"
     else
       flash[:danger] = "Unable to Get response from Azure"
     end
